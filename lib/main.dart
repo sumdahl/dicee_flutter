b7661f98f4fbeb438f1376de3a1974d46d346b3b
sumdahl <sumirandahal46@gmail.com>
2022-04-06 19:04:10 +0545

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+int counter = 5;
+
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(
+            'Dice',
+            style: TextStyle(fontSize: 25, fontFamily: 'Poppins'),
+          ),
           backgroundColor: Colors.red,
+          centerTitle: true,
         ),
         body: DicePage(),
       ),
@@ -18,6 +24,26 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    changeDice() {}
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: changeDice(),
+              child: Image.asset(
+                'images/dice1.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Image.asset('images/dice2.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
